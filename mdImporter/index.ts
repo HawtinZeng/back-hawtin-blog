dd2006ad879fb58d3d7d2f73557a0c12142bde54
zenghawtin <1448593512@qq.com>
2025-01-25 12:00:56 +0800

    feat: finish markdown files importer

---
+++ mdImporter/index.ts
@@ -0,0 +1,46 @@
+import * as fs from "fs";
+import { join, sep } from "path";
+import { Mongo } from "./mongodb";
+function main() {
+  const m = new Mongo("mongodb://120.53.232.99:27017/", "hawtin-blog", "blogs");
+
+  const args = process.argv.slice(2);
+  const path = args[0];
+  const markdowns = traversalFiles(path).filter((f) => f.endsWith(".md"));
+
+  const ps: Promise<any>[] = [];
+  markdowns.forEach((item: string) => {
+    const content = fs.readFileSync(item, "utf8");
+    const slices = item.split(sep);
+    const title = slices[slices.length - 2];
+
+    const insert = m.collection.insertOne({
+      title,
+      content,
+    });
+    ps.push(insert);
+  });
+  Promise.all(ps).then(() => {
+    m.close();
+  });
+}
+
+function traversalFiles(path) {
+  const stats = fs.statSync(path);
+  if (stats.isDirectory()) {
+    const files = fs.readdirSync(path);
+    const filePath = files.map((f) => join(path, f));
+
+    let filesDestination = [];
+    filePath.forEach((f) => {
+      const nested = traversalFiles(f);
+      filesDestination = filesDestination.concat(nested);
+    });
+
+    return filesDestination;
+  } else {
+    return [path];
+  }
+}
+
+main();
