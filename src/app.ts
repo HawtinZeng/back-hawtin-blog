53fa1d41efc4034c2ae3717004813d5582024f81
zenghawtin <1448593512@qq.com>
2025-02-21 00:36:10 +0800

    chores: logger for build env

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import cors from "@fastify/cors";
 
 const server = fastify({
   // Logger only for production
-  logger: !!(process.env.NODE_ENV === "development"),
+  logger: !!(process.env.NODE_ENV !== "development"),
 });
 
 // Middleware: Router
